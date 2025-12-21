8fc848013b73800a7d440f82b0098fb54cf2f737
Lukas Simonson <lukas@hydradesignllc.com>
2025-12-20 22:13:12 -0700

    Adds Architecture Packages

---
+++ Data/Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Data",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Data",
+            targets: ["Data"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "../Business")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Data",
+            dependencies: [
+                .product(name: "Business", package: "Business")
+            ]
+        ),
+        .testTarget(
+            name: "DataTests",
+            dependencies: ["Data"]
+        ),
+    ]
+)
