6b6080ac267cf911fc0b3eeba5cff016016506db
Lukas Simonson <lukas@simonson.co>
2026-06-19 12:32:33 -0600

    chore(auth): update `CobwebRemoteAuthDatasource` to use the `catchNetwork` helper

--- Data/Sources/Data/Auth/DataSources/CobwebRemoteAuthDataSource.swift
+++ Data/Sources/Data/Auth/DataSources/CobwebRemoteAuthDataSource.swift
@@ -13,73 +13,31 @@ public struct CobwebRemoteAuthDataSource: RemoteAuthDataSourceContract {
     }
     
     public func login(baseURL: String, username: String, password: String) async throws(CoreError) -> AuthSession {
-        do {
-            let response = try await Cobweb.URL.using(baseURL: baseURL)
+        try await CoreError.catchNetwork(performing: "logging in user", logger: logger, feature: .auth) {
+            let user = try await Cobweb.URL.using(baseURL: baseURL)
                 .path("/api/auth/login").get()
                 .also { logger.info("Sending Request to GET /api/auth/login") }
                 .withHeaders(.contentType(value: "application/json"), .basicAuth(username: username, password: password))
                 .response()
-
-            switch try response.statusCode {
-                case 200: break // Expected Response
-                case 401: throw CoreError.invalidCredentials
-                case 500...599: throw CoreError.serverError(layer: .data, feature: .auth)
-                default: throw CoreError.unexpectedResponse(
-                    layer: .data,
-                    feature: .auth,
-                    code: try response.statusCode
-                )
-            }
-
-            let user = try response.body(as: UserDTO.self)
-
+                .verifyStatusCode(isNot: 401, orThrow: CoreError.invalidCredentials)
+                .withStatusCoreError(expecting: 200, feature: .auth)
+                .body(as: UserDTO.self)
+            
             return AuthSession(
                 user: user.toCore(at: baseURL),
-                // The user.token SHOULD always be present.
                 token: AuthToken(token: user.token!, createdAt: .now)
             )
-        } catch let error as CoreError {
-            throw error
-        } catch let error as Cobweb.URL.URLError {
-            logger.error("Unable to create URL", for: error)
-            throw CoreError.urlError(layer: .data, feature: .auth)
-        } catch let error as Cobweb.HTTP.Request.ResponseError {
-            logger.error("Login failed with response error", for: error)
-            throw CoreError.invalidResponseFormat(layer: .data, feature: .auth)
-        } catch {
-            logger.error("Unknown error found during login", for: error)
-            throw CoreError.unexpected(layer: .data, feature: .auth)
         }
     }
     
     public func verifyServer(baseURL: String) async throws(CoreError) {
-        do {
-            let response = try await Cobweb.URL.using(baseURL: baseURL)
+        try await CoreError.catchNetwork(performing: "verifying server status", logger: logger, feature: .auth) {
+            try await Cobweb.URL.using(baseURL: baseURL)
                 .path("/api/info")
                 .also { logger.info("Sending Request to GET /api/info") }
                 .get()
                 .response()
-            
-            switch try response.statusCode {
-                case 200: break
-                case 500...599: throw CoreError.serverError(layer: .data, feature: .auth)
-                default: throw CoreError.unexpectedResponse(
-                    layer: .data,
-                    feature: .auth,
-                    code: try response.statusCode
-                )
-            }
-        } catch let error as CoreError {
-            throw error
-        } catch let error as Cobweb.URL.URLError {
-            logger.error("Unable to create URL", for: error)
-            throw CoreError.urlError(layer: .data, feature: .auth)
-        } catch let error as Cobweb.HTTP.Request.ResponseError {
-            logger.error("Server Verification failed with response error", for: error)
-            throw CoreError.invalidResponseFormat(layer: .data, feature: .auth)
-        } catch {
-            logger.error("Unknown error found during server verification", for: error)
-            throw CoreError.unexpected(layer: .data, feature: .auth)
+                .withStatusCoreError(expecting: 200, feature: .auth)
         }
     }
 }
